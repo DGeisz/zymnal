69edfbb015f55f427e79c5a57f01c97d69d268a9
Danny Geisz <dannygeisz@playdigitari.com>
2022-07-17 18:22:22 +0200

    Added basic promise utils

---
+++ src/global_utils/promise_utils.ts
@@ -0,0 +1,31 @@
+import { EventEmitter } from "stream";
+import { threadId } from "worker_threads";
+
+export class ControlledAwaiter {
+  ee?: EventEmitter;
+  triggered = false;
+
+  trigger = () => {
+    if (this.ee) {
+      this.ee.emit("trigger");
+    }
+
+    this.triggered = true;
+  };
+
+  awaitTrigger = async () => {
+    if (!this.ee) {
+      this.ee = new EventEmitter();
+    }
+
+    if (this.triggered) {
+      return;
+    } else {
+      return new Promise((resolve) => this.ee?.once("trigger", resolve));
+    }
+  };
+
+  reset = () => {
+    this.triggered = false;
+  };
+}
